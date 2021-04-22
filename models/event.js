349b3984513fdc9731b0934a65a9983e06fb5c1f
JStameus <joseph.stameus@gmail.com>
2021-04-22 22:40:19 +0200

    Started implementing a model/view/controller scheme and added events and calendarinfo json files to act as a placeholder database

---
+++ models/event.js
@@ -0,0 +1,43 @@
+import fs from "fs";
+import path from "path";
+
+const events = JSON.parse(fs.readFileSync(path.resolve("./data/events.json")));
+
+function randomID() {
+    let id = "";
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "1234567890";
+    // Get three random letters and two random numbers
+    for(let i = 0; i < 3; i++) {
+        id += letters[Math.floor(Math.random() * letters.length)];
+    }
+    for(let i = 0; i < 2; i++) {
+        id += numbers[Math.floor(Math.random() * numbers.length)];
+    }
+    return id;
+}
+
+function findAll() {
+    return events;
+}
+
+function addNew(body) {
+    try {
+        const newEvent = {
+            id: randomID(),
+            // TODO: It probably shouldn't be possible to not set a name. Make
+            // sure to handle this properly.
+            name: body.name || "Unnamed",
+            description: body.description || "No description",
+            date: body.date || "N/A",
+            startTime: body.startTime || "N/A",
+            endTime: body.endTime || "N/A",
+        }
+    }
+    catch(err) {
+        console.log(`ERROR: {err}`);
+        return false;
+    }
+}
+
+export default {randomID, findAll, addNew};
