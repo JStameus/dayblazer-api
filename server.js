eba571ff31da53409d87bf010a4283e302c904a0
JStameus <joseph.stameus@gmail.com>
2021-04-22 18:23:32 +0200

    Removed index.html and started using handlebars instead. Updated package.json with nodemon dev dependency.

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 import express from "express";
+import handlebars from "express-handlebars";
 import path from "path";
 import fs from "fs";
 
 const app = express();
+
+// -- SERVER SETUP --
 const PORT = 3000;
+// TODO: This directory works, but it goes all the way from "home". Find a way
+// to get the absolute path of the project root only.
+const PUBLICDIR = path.resolve("./public");
 
-app.use(express.static(path.resolve("./public")));
-
-const routeMap = [
-    {
-        route: "/",
-        filePath: "public/index.html",
-    }
-]
+app.use(express.static(PUBLICDIR));
+app.set("view engine", "handlebars");
+app.engine("handlebars", handlebars({
+    layoutsDir: path.resolve("./views/layouts"),
+    partialsDir: path.resolve("./views/partials"),
+    defaultLayout: "index",
+    extname: "handlebars",
+}));
 
 // -- ROUTES --
+app.get("/", (req, res) => {
+    res.render("main", {message: "Welcome, USERNAME!"});
+})
 
 // -- INITIALIZATION --
 app.listen(PORT, () => {
     console.log(`Dayblazer Calendar: Server listening on Port ${PORT}`);
+    console.log(`Serving static files from: ${PUBLICDIR}`);
 });
