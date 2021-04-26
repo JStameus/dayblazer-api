35c1626250f32527e681ff78a0b906034e977886
JStameus <joseph.stameus@gmail.com>
2021-04-26 14:54:01 +0200

    Started redoing design and fixed sass folder structure

--- public/app.js
+++ public/app.js
@@ -1,58 +1,15 @@
-// NOTE: Most of this is for testing purposes. A more dynamic solution will have
-// to be implemented when the back-end is up and running.
-let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-]
-let currentMonth = months[3];
-let currentWeek = "W 16";
-
-const viewModes = {
-    week: "Week",
-    month: "Month",
-    year: "Year"
-}
-let currentViewMode = viewModes.week;
-
-function updateWeekView() {
-    const currentDisplay = document.querySelector("#header_viewControl_current");
-    currentDisplay.textContent = currentWeek;
+function daysInMonth(month, year) {
+    return new Date(year, month, 0).getDate();
 }
 
-function updateMonthView() {
-    const currentDisplay = document.querySelector("#header_viewControl_current");
-    currentDisplay.textContent = currentMonth;
-}
-
-function updateYearView() {
-
-}
-
-function init() {
-    switch(currentViewMode) {
-        case "Week":
-            console.log("View Mode: WEEK");
-            updateWeekView();
-            break;
-        case "Month":
-            console.log("View Mode: MONTH");
-            updateMonthView();
-            break;
-        case "Year":
-            console.log("View Mode: YEAR");
-            updateYearView();
-            break;
+function createDayDivs() {
+    const dayCount = daysInMonth(4, 2021);
+    for(i = 0; i < dayCount; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("mainView_day");
     }
 }
 
-init();
+function init() {
+    
+}
